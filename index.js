61614d4840ec023dea8328ce0370d8ce74018dfd
Jeon Ji Young <jeonji_0628@naver.com>
2019-01-19 11:10:44 +0900

    add index.js for function handlehome, handleProfiles
    ;

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("g2");
+const express = require("express")
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening in on : http://localhost:${PORT}`);
+
+}
+
+function handleHome(req, res){
+    res.send("Hello from home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+app.listen(4000, handleListening);
